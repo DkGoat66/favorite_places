2448cc1363fbcd90fbe4c5e3bb6fb3674f39a96c
Diwakar Rai <111923854+DkGoat66@users.noreply.github.com>
2024-10-10 20:12:04 -0400

    Update places.dart

--- screens/places.dart
+++ screens/places.dart
@@ -1,7 +1,7 @@
 import 'package:favorite_places/screens/add_place.dart';
 import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
-
+// Stateless widget representing the main screen where a list of favorite places is shown.
 class PlacesScreen extends StatelessWidget {
   const PlacesScreen({super.key});
 
